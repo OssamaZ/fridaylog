fe1a9475d8571861519970efff043afb371e93aa
Ossama Zaid <w4rz4zi@gmail.com>
2023-09-18 20:41:35 +0100

    Fix issues with prisma generated files in workspaces

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,3 +1,16 @@
-module.exports = {
+// https://www.prisma.io/docs/guides/other/troubleshooting-orm/help-articles/nextjs-prisma-client-monorepo
+const { PrismaPlugin } = require("@prisma/nextjs-monorepo-workaround-plugin");
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   reactStrictMode: true,
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.plugins = [...config.plugins, new PrismaPlugin()];
+    }
+
+    return config;
+  },
 };
+
+module.exports = nextConfig;
--- apps/web/src/pages/api/hello.ts
+++ apps/web/src/pages/api/hello.ts
@@ -1,10 +1,13 @@
-import { NextApiRequest, NextApiResponse } from "next";
-import { prisma } from "@fridaylog/prisma";
+import { type NextApiRequest, type NextApiResponse } from "next";
+import { prisma } from "@fridaylog/database";
 
 export default async function handler(_: NextApiRequest, res: NextApiResponse) {
   const examples = await prisma.example.findMany();
-  const message = examples[0]?.name || "World";
+  const message = examples[0].name || "World";
   res.status(200).json({
     data: `Hello ${message}`,
   });
+  // res.status(200).json({
+  //   data: "Hello world",
+  // });
 }

1bee2559f8ee3905c962460229cb854423238277
Ossama Zaid <w4rz4zi@gmail.com>
2023-09-18 17:27:58 +0100

    Test hooking up prisma in next api

---
+++ apps/web/src/pages/api/hello.ts
@@ -0,0 +1,10 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { prisma } from "@fridaylog/prisma";
+
+export default async function handler(_: NextApiRequest, res: NextApiResponse) {
+  const examples = await prisma.example.findMany();
+  const message = examples?.[0]?.name || "World";
+  return res.status(200).json({
+    data: `Hello ${message}`,
+  });
+}
--- apps/web/src/pages/index.tsx
+++ apps/web/src/pages/index.tsx
@@ -1,9 +1,23 @@
 import { Card } from "@fridaylog/ui";
+import { useEffect, useState } from "react";
 
 export default function Page(): JSX.Element {
+  const [data, setData] = useState("Loading ..");
+  useEffect(() => {
+    (async function getHello() {
+      try {
+        const resp = await fetch("/api/hello");
+        const data = await resp.json();
+        setData(data.data);
+      } catch (error) {
+        setData("Error");
+      }
+    })();
+  }, []);
+
   return (
     <main className="flex min-h-screen w-full items-center justify-center bg-slate-50">
-      <Card>Hello World</Card>
+      <Card>{data}</Card>
     </main>
   );
 }

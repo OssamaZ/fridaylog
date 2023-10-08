3396db3d5bff17c869e9cb6a426ecad3a7c90391
Ossama Zaid <w4rz4zi@gmail.com>
2023-10-08 10:55:28 +0100

    Update prisma to use neon serverless driver

--- packages/database/src/client.ts
+++ packages/database/src/client.ts
@@ -1,10 +1,18 @@
+import { Pool, neonConfig } from "@neondatabase/serverless";
+import { PrismaNeon } from "@prisma/adapter-neon";
 import { PrismaClient } from "@prisma/client";
+import ws from "ws";
+import dotenv from "dotenv";
+
+dotenv.config();
+neonConfig.webSocketConstructor = ws;
 
 declare global {
   var prisma: PrismaClient | undefined;
 }
-
-export const prisma = global.prisma || new PrismaClient();
+const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+const adapter = new PrismaNeon(pool);
+export const prisma = global.prisma || new PrismaClient({ adapter });
 
 if (process.env.NODE_ENV !== "production") global.prisma = prisma;
 
